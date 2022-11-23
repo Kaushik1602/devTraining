c3f965c26a69ecd897e3bb0bea45e790bfc47986
Kaushik1602 <bankarkaushik@gmail.com>
2022-11-23 13:57:09 +0530

    added controller

--- boot/phonems/src/main/java/com/example/phonems/controllers/Controller.java
+++ boot/phonems/src/main/java/com/example/phonems/controllers/Controller.java
@@ -1,9 +1,55 @@
 package com.example.phonems.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.phonems.entity.Contact;
+import com.example.phonems.service.IService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class Controller {
-    
+
+    @Autowired
+    IService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/customers/add")
+    private Contact add(@RequestBody Contact contact){
+        return service.addContact(contact);
+    }
+
+    @ResponseStatus(HttpStatus.FOUND)
+    @GetMapping("/customers/view")
+    private List<Contact> viewAll(){
+        return service.displayAll();
+    }
+
+    @ResponseStatus(HttpStatus.FOUND)
+    @GetMapping("/customers/findByNumber/{phoneNumber}")
+    private Contact findByNumber(@PathVariable long phoneNumber){
+        return service.searchContactByGivenPhoneNo(phoneNumber);
+    }
+
+    @ResponseStatus(HttpStatus.FOUND)
+    @GetMapping("/customers/findByName/{firstName}")
+    private List<Contact> findByFirstName(@PathVariable String firstName){
+        return service.searchContactByFirstName(firstName);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/customers/delete/{phoneNumber}")
+    private Contact deleteByNumber(@PathVariable long phoneNumber){
+        return service.removeContact(phoneNumber);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/customers/update/{phoneNumber}")
+    private Contact updateEmail(@PathVariable long phoneNumber, @RequestBody String emailId){
+        return service.updateEmail(phoneNumber,emailId);
+    }
+
+
 
 }
