a7e01b6e1a19d7ec1ff5c59db017ecc06aae13dd
MaheshRedekar <redekarm7@gmail.com>
2022-11-25 00:56:01 +0530

    validation added for update.

--- boot/phonems/src/main/java/com/example/phonems/controllers/Controller.java
+++ boot/phonems/src/main/java/com/example/phonems/controllers/Controller.java
@@ -16,40 +16,39 @@ public class Controller {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/customers/add")
-    private String add(@RequestBody Contact contact){
+    private String add(@RequestBody Contact contact) {
         return service.addContact(contact);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/customers/view")
-    private List<Contact> viewAll(){
+    private List<Contact> viewAll() {
         return service.displayAll();
     }
 
     @ResponseStatus(HttpStatus.FOUND)
     @GetMapping("/customers/findByNumber/{phoneNumber}")
-    private Contact findByNumber(@PathVariable long phoneNumber){
+    private Contact findByNumber(@PathVariable long phoneNumber) {
         return service.searchContactByGivenPhoneNo(phoneNumber);
     }
 
     @ResponseStatus(HttpStatus.FOUND)
     @GetMapping("/customers/findByName/{firstName}")
-    private List<Contact> findByFirstName(@PathVariable String firstName){
+    private List<Contact> findByFirstName(@PathVariable String firstName) {
         return service.searchContactByFirstName(firstName);
     }
 
     @ResponseStatus(HttpStatus.ACCEPTED)
     @DeleteMapping("/customers/delete/{phoneNumber}")
-    private Contact deleteByNumber(@PathVariable long phoneNumber){
+    private String deleteByNumber(@PathVariable long phoneNumber) {
         return service.removeContact(phoneNumber);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PutMapping("/customers/update/{phoneNumber}")
-    private Contact updateEmail(@PathVariable long phoneNumber, @RequestBody String emailId){
-        return service.updateEmail(phoneNumber,emailId);
+    private String updateEmail(@PathVariable long phoneNumber, @RequestBody String emailId) {
+        return service.updateEmail(phoneNumber, emailId);
     }
-
 
 
 }
