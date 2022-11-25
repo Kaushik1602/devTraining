b9100afec2d436f9c3041f6dac9fb8b8fe445ba9
MaheshRedekar <redekarm7@gmail.com>
2022-11-25 16:07:25 +0530

    changes in exception

--- boot/phonems/src/main/java/com/example/phonems/service/ServiceImpl.java
+++ boot/phonems/src/main/java/com/example/phonems/service/ServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.phonems.service;
 
 import com.example.phonems.entity.Contact;
 import com.example.phonems.exceptions.EnterValidDataException;
+import com.example.phonems.exceptions.NoNotFoundException;
 import com.example.phonems.validations.Validation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,9 +20,8 @@ public class ServiceImpl implements IService {
 
 
     @Override
-    public String addContact(Contact contact) throws Exception{
+    public String addContact(Contact contact) throws Exception {
         Contact contactDetails = new Contact();
-
         try {
             if (!validation.validateFirstName(contact.getFirstName()))
                 throw new EnterValidDataException("Enter valid Name");
@@ -36,17 +36,16 @@ public class ServiceImpl implements IService {
             contactDetails.setEmailId(contact.getEmailId());
 
             if (!validation.validateNumber(contact.getPhoneNumber()))
-                throw new EnterValidDataException("Enter valid Number "+contact.getPhoneNumber());
+                throw new EnterValidDataException("Enter valid Number " + contact.getPhoneNumber());
 
             contactDetails.setPhoneNumber(contact.getPhoneNumber());
 
             if (!validation.validateAge(contact.getAge()))
                 throw new EnterValidDataException("Enter valid Age");
             contactDetails.setAge(contact.getAge());
-        }catch (EnterValidDataException e){
+        } catch (EnterValidDataException e) {
             return e.getMessage();
         }
-
         contactList.add(contactDetails);
         return "Contact Added";
     }
@@ -82,17 +81,28 @@ public class ServiceImpl implements IService {
     @Override
     public String removeContact(long phoneNumber) throws Exception {
         String string = String.valueOf(phoneNumber);
-        if (string.length() != 10) {
-            return "enter valid number";
-//            throw new EnterValidNumberException("enter valid number"+ phoneNumber);
-        }
-        for (Contact contact : contactList) {
-            if (contact.getPhoneNumber() == phoneNumber) {
-                contactList.remove(contact);
-                return "contact removed.";
+        String result = null;
+        Contact contact1=new Contact();
+        try {
+            if (string.length() != 10) {
+                throw new EnterValidDataException("enter valid phone number");
             }
+            for (Contact contact : contactList) {
+                if (contact.getPhoneNumber() == phoneNumber) {
+                    contact1=contact;
+                    result = "found..";
+                }
+            }
+            if (result == null) {
+                throw new NoNotFoundException("number not found");
+            }
+        } catch (EnterValidDataException e) {
+            return e.getMessage();
+        } catch (NoNotFoundException e) {
+            return e.getMessage();
         }
-        return "Phone number not found.";
+        contactList.remove(contact);
+        return "contact removed successfully.";
     }
 
     @Override
